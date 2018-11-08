7510996a515deb03f5b9d8cd1d8b0a130538af8f
Daniel Zuziak <daniel.zuziak@gmail.com>
2018-11-08 18:59:01 +0100

    Fixed function approximation (units: mm->m mH->H)

--- approximate.m
+++ approximate.m
@@ -3,9 +3,9 @@ data = csvread('inductance.csv', row_offset);
 inductance = data(:, 1);
 distance = data(:, 2);
 fit_type = fittype( @(a,b,c,x) a+b*exp(c*x), 'independent', 'x', 'coefficients', {'a', 'b', 'c'});
-start_point = [1, 1, -1];
+start_point = [0.1, 0.1, -250];
 
-fit_function = fit(data(:, 2), inductance, fit_type, 'StartPoint', start_point);
+fit_function = fit(distance, inductance, fit_type, 'StartPoint', start_point);
 
 hold on;
 plot(distance, fit_function(distance));
