0db9400e5e36f7f7d5ba524c5f2112e3b5a592ea
Daniel Zuziak <daniel.zuziak@gmail.com>
2018-11-08 19:05:26 +0100

    What do you think about the comments?

--- get_AB.m
+++ get_AB.m
@@ -2,14 +2,10 @@ function [A, B] = get_AB(R,m,L,x1,x2,x3)
 
 approximate;
 
-cj = differentiate(fit_function, x1);
+cj = differentiate(fit_function, x1); % Czy to o to chodzi?
 
-A = [0, 1, 0;
-    x3^2*cj/(2*m), 0, x3*cj/m;
-    0, -x3*cj/L, -R/L - x2*cj/L];
+A = [0,  1,  0;
+    x3^2*cj/(2*m),  0,  x3*cj/m;
+    0,  -x3*cj/L,  -R/L-x2*cj/L];
 
-
-B = [0, 0, 1/L];
-
-
-% Policzyc prokxymacje dla henrow a nie mH
+B = [0, 0, 1/L]; % B nie powinno byc [0; 0; 1/L]?
